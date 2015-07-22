a67ea7496d6835574558b6fe7744baa20a13ae8a
Hackbright Student <info@hackbrightacademy.com>
2015-07-22 16:33:51 -0700

    finished debugging

--- shoppingsite.py
+++ shoppingsite.py
@@ -7,7 +7,7 @@ Authors: Joel Burton, Christian Fernandez, Meggie Mahnken.
 """
 
 
-from flask import Flask, render_template, redirect, flash
+from flask import Flask, render_template, redirect, flash, session
 import jinja2
 
 import model
@@ -63,6 +63,8 @@ def shopping_cart():
     # TODO: Display the contents of the shopping cart.
     #   - The cart is a list in session containing melons added
 
+
+
     return render_template("cart.html")
 
 
@@ -76,6 +78,8 @@ def add_to_cart(id):
 
     # TODO: Finish shopping cart functionality
     #   - use session variables to hold cart list
+
+    item = request.form.get("display_melon.id", "[]")
 
     return "Oops! This needs to be implemented!"
 
