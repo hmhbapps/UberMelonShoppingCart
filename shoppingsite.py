78861267abc9cd0eae3565555b1b5a03d248c368
Hackbright Student <info@hackbrightacademy.com>
2015-07-22 17:53:05 -0700

    last commit of day yay

--- shoppingsite.py
+++ shoppingsite.py
@@ -7,7 +7,7 @@ Authors: Joel Burton, Christian Fernandez, Meggie Mahnken.
 """
 
 
-from flask import Flask, render_template, redirect, flash, session
+from flask import Flask, render_template, redirect, flash, session, request
 import jinja2
 
 import model
@@ -43,14 +43,14 @@ def list_melons():
                            melon_list=melons)
 
 
-@app.route("/melon/<int:id>")
-def show_melon(id):
+@app.route("/melon/<int:melon_id>")
+def show_melon(melon_id):
     """Return page showing the details of a given melon.
 
     Show all info about a melon. Also, provide a button to buy that melon.
     """
 
-    melon = model.Melon.get_by_id(id)
+    melon = model.Melon.get_by_id(melon_id)
     print melon
     return render_template("melon_details.html",
                            display_melon=melon)
@@ -63,13 +63,11 @@ def shopping_cart():
     # TODO: Display the contents of the shopping cart.
     #   - The cart is a list in session containing melons added
 
-
-
     return render_template("cart.html")
 
 
-@app.route("/add_to_cart/<int:id>")
-def add_to_cart(id):
+@app.route("/add_to_cart/<int:melon_id>")
+def add_to_cart(melon_id):
     """Add a melon to cart and redirect to shopping cart page.
 
     When a melon is added to the cart, redirect browser to the shopping cart
@@ -79,7 +77,8 @@ def add_to_cart(id):
     # TODO: Finish shopping cart functionality
     #   - use session variables to hold cart list
 
-    item = request.form.get("display_melon.id", "[]")
+    session['melon_id'] = melon_id
+    print session['melon_id']
 
     return "Oops! This needs to be implemented!"
 
